8e1b95fa8f233fc57dcfa2437482248b42d2de52
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-05-23 19:55:44 +0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
-import App from "./components/App/App.tsx";
+// import App from "./components/App/App.tsx";
 import "modern-normalize";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
   </StrictMode>
 );
